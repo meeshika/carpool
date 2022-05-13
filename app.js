8f6702730d53dfc776881b2d3d3595b0c3dd0a2d
Meeshika Garg <gargmeeshika@gmail.com>
2022-05-13 14:21:48 +0530

    make it better

--- app.js
+++ app.js
@@ -42,12 +42,16 @@ app.use((req, res, next) => {
 app.use("/trips", tripRoutes);
 app.use("/orders", orderRoutes);
 app.use("/user", userRoutes);
+app.get("/",(req,res)=>{
+  return res.status(200).json({message:"api connected succefully"})
+})
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
+
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 4550;
+const port = process.env.port||4550;
 
 const server = http.createServer(app);
 
