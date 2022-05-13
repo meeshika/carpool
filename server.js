0bbf9cc16c0cfd36188072fc165193de291df0bd
Meeshika Garg <gargmeeshika@gmail.com>
2022-05-13 20:49:57 +0530

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const app = require('./app');
 
-const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
+const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env;
+const server = http.createServer(app);
 server.listen(PORT, LOCAL_ADDRESS, () => {
   const address = server.address();
-  const server = http.createServer(app);
+//  const server = http.createServer(app);
 
   console.log('server listening at', address);
 });
