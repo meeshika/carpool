d42abc02f64a5a099e0dce80d75404bc70fdbfcb
Meeshika Garg <gargmeeshika@gmail.com>
2022-05-14 13:55:34 +0530

    make it better

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -91,20 +91,20 @@ exports.user_login = (req, res, next) => {
           });
         }
         if (result) {
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0]._id
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h"
-            }
-          );
+          // const token = jwt.sign(
+          //   {
+          //     email: user[0].email,
+          //     userId: user[0]._id
+          //   },
+          //   process.env.JWT_KEY,
+          //   {
+          //     expiresIn: "1h"
+          //   }
+          // );
           console.log(req.body.email);
           return res.status(200).json({
             message: "Auth successful",
-            token: token
+          //  token: token
           });
         }
         console.log(req.body.email);
