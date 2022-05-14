d103964449fa08c3993e734214b821490605a93c
Meeshika Garg <gargmeeshika@gmail.com>
2022-05-14 13:21:10 +0530

    make it better

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+//require("dotenv").config();
 const User = require("../models/user");
 
 // exports.user_signup = (req, res, next) => {
@@ -27,43 +27,7 @@ const User = require("../models/user");
 //   User.find({ email: req.body.email })
 //     .exec()
 //     .then(user => {
-//       if (user.length < 1) {
-//         return res.status(401).json({
-//           message: "Auth failed"
-//         });
-//       }
-//          if (user[0].password !== req.body.password) {
-//           return res.status(401).json({
-//             message: "Auth failed"
-//           });
-//         }
-//         if (result) {
-//           const token = jwt.sign(
-//             {
-//               email: user[0].email,
-//               userId: user[0]._id
-//             },
-//             process.env.JWT_KEY,
-//             {
-//               expiresIn: "1h"
-//             }
-//           );
-//           return res.status(200).json({
-//             message: "Auth successful",
-//             token: token
-//           });
-//         }
-//         res.status(401).json({
-//           message: "Auth failed"
-//         });
-//       })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     });
-// };
+//       if (user.length < 1)
 
 exports.user_signup = (req, res, next) => {
   User.find({ email: req.body.email })
